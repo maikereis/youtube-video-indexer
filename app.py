4261313ef091c870b12a633929ebc828939407e4
maike reis <maikerdralcantara@gmail.com>
2025-05-11 18:42:53 -0300

    add message queue

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.errors import RateLimitExceeded
 
-import xml.etree.ElementTree as ET
+from notifications import NotificationQueue
+
+MESSAGE_QUEUE_NAME = "yt_queue"
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "pubsubhubbub.appspot.com", "*.ngrok-free.app"]
 
 limiter = Limiter(
     key_func=get_remote_address,
@@ -15,20 +18,21 @@ limiter = Limiter(
 
 app = FastAPI()
 
+notification_queue = NotificationQueue(MESSAGE_QUEUE_NAME)
+
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=["localhost", "127.0.0.1", "pubsubhubbub.appspot.com", "*.ngrok-free.app"]
+    allowed_hosts=ALLOWED_HOSTS
 )
-
 
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
-
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
+
 
 @app.get("/webhooks")
 @limiter.limit("10/minute")  # Example: limit to 10 requests per minute
@@ -39,39 +43,12 @@ async def verify_subscription(request: Request):
         return Response(content=hub_challenge, media_type="text/plain")
     return Response(status_code=400)
 
-def handle_response(xml_data: str):
-    try:
-        # Parse the XML content
-        root = ET.fromstring(xml_data)
-
-        # Define namespaces
-        namespaces = {
-            "atom": "http://www.w3.org/2005/Atom",
-            "yt": "http://www.youtube.com/xml/schemas/2015",
-        }
-
-        # Find the entry element
-        entry = root.find("atom:entry", namespaces)
-        if entry is not None:
-            video_id = entry.find("yt:videoId", namespaces).text
-            channel_id = entry.find("yt:channelId", namespaces).text
-            title = entry.find("atom:title", namespaces).text
-            published = entry.find("atom:published", namespaces).text
-            updated = entry.find("atom:updated", namespaces).text
-
-            # Process the extracted information as needed
-            print(f"New video uploaded: {title} (ID: {video_id}) on channel {channel_id}")
-            print(f"Published at: {published}, Updated at: {updated}")
-        else:
-            print("No entry found in the feed.")
-
-    except ET.ParseError as e:
-        print(f"Failed to parse XML: {e}")
 
 @app.post("/webhooks")
 @limiter.limit("5/minute")  # Example: limit to 5 requests per minute
 async def handle_notification(request: Request):
     # Handle content update
     content = await request.body()
-    handle_response(content)
+    xml_data = content.decode("utf-8")
+    notification_queue.enqueue(xml_data)
     return Response(status_code=200)
