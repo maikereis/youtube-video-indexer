009acce9340739c15eb7337ae469091c7717a948
maike reis <maikerdralcantara@gmail.com>
2025-05-24 13:46:57 -0300

    update YouTubeNotificationProcessor instantiation

--- services/worker/main.py
+++ services/worker/main.py
@@ -1,15 +1,30 @@
 import asyncio
 
+from ytindexer.config import settings
+from ytindexer.database import ValkeyConnection
 from ytindexer.queues import NotificationQueue
-from ytindexer.worker import YouTubeNotificationProcessor
+from ytindexer.worker import (YouTubeNotificationParser,
+                              YouTubeNotificationProcessor)
 
 
 async def main():
-    notification_queue = NotificationQueue(queue_name="notification_queue")
-    metadata_queue = NotificationQueue(queue_name="metadata_queue")
 
-    indexer = YouTubeNotificationProcessor(notification_queue, metadata_queue)
-    await indexer.run()
+    valkey_client = ValkeyConnection(
+        host=settings.valkey.host,
+        port=settings.valkey.port,
+        password=settings.valkey.password.get_secret_value(),
+    )
+
+    valkey_conn = await valkey_client.connect()
+
+    notification_queue = NotificationQueue(valkey_conn, "notification-queue")
+    output_queue = NotificationQueue(valkey_conn, "output-queue")
+    parser = YouTubeNotificationParser()
+
+    processor = YouTubeNotificationProcessor(notification_queue, output_queue, parser)
+
+    await processor.run()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
